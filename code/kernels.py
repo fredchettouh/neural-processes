b960f570fd2ccbca5e26a145908acdad8ba864fa
fredchettouh <frederik@Frederiks-MBP.fritz.box>
2020-04-27 19:09:03 +0200

    creating clean slate before branching out to implement new kernel

--- code/kernels.py
+++ code/kernels.py
@@ -1,16 +1,16 @@
-# Expand and take the difference
-    xdata1 = tf.expand_dims(xdata, axis=1)  # [B, 1, num_total_points, x_size]
-    xdata2 = tf.expand_dims(xdata, axis=2)  # [B, num_total_points, 1, x_size]
-    diff = xdata1 - xdata2  # [B, num_total_points, num_total_points, x_size]
+import torch
 
-    # [B, y_size, num_total_points, num_total_points, x_size]
-    norm = tf.square(diff[:, None, :, :, :] / l1[:, :, None, None, :])
 
-    norm = tf.reduce_sum(
-        norm, -1)  # [B, data_size, num_total_points, num_total_points]
-
-    # [B, y_size, num_total_points, num_total_points]
-    kernel = tf.square(sigma_f)[:, :, None, None] * tf.exp(-0.5 * norm)
-
-    # Add some noise to the diagonal to make the cholesky work.
-    kernel += (sigma_noise**2) * tf.eye(num_total_points)
+def tensor_rbf(x1, x2, l1_scale, simga_f_scale, sigma_noise, dimy=1):
+    batch_size = x1.shape[0]
+    num_points = x1.shape[1]
+    dimx = x1.shape[2]
+    x1 = x1.unsqueeze(1)
+    x2 = x2.unsqueeze(2)
+    diff = x1 - x2
+    l1 = torch.ones(batch_size, dimy, dimx) * l1_scale
+    sigma_f = torch.ones(batch_size, dimy) * simga_f_scale
+    norm = (diff[:, None, :, :, :] / l1[:, :, None, None, :]).pow(2).mean(-1)
+    kernel = sigma_f.pow(2)[:, :, None, None] * (-0.5 * norm).pow(2)
+    kernel += (sigma_noise ** 2) * torch.eye(num_points)
+    return kernel
