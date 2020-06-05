fixed confident intervals by sorting the context values target values, mu values and variance values inside the plotting function

cnp/helpers.py
@@ -46,14 +46,15 @@ class Helper:
             arg_list.append(arg)
         return arg_list
 
-    # @staticmethod
-    # def create_loader(datagenerator_instance, num_instances, noise, length_scale, gamma, batch_size):
-    #     x_values, func_x = datagenerator_instance.generate_curves(num_instances, noise, length_scale, gamma)
-    #     func_x = Helper.list_np_to_sensor(func_x)
-    #     x_values = x_values.repeat(func_x.shape[0], 1, 1)
-    #     dataset = data.TensorDataset(x_values, func_x)
-    #     dataloader = data.DataLoader(dataset, batch_size=batch_size)
-    #     return dataloader
+    @staticmethod
+    def sort_arrays(prin_array, *args):
+        order_permutation = prin_array.argsort()
+        prin_array = prin_array[order_permutation]
+        sorted_arrays = [prin_array]
+        for arg in args:
+            sorted_arrays.append(arg[order_permutation])
+        return sorted_arrays
+
 
     @staticmethod
     def create_loader(x_values, func_x, batch_size):
@@ -61,7 +62,6 @@ class Helper:
         dataloader = data.DataLoader(dataset, batch_size=batch_size)
         return dataloader
 
-    @staticmethod
     @staticmethod
     def list_np_to_sensor(list_of_arrays, stack=True):
         if stack:
@@ -101,23 +101,31 @@ class Plotter:
         plt.close()
 
     @staticmethod
-    def plot_run(batch_size, contxt_idx, xvalues, funcvalues, target_y,
+    def plot_run(contxt_idx, xvalues, funcvalues, target_y,
                  target_x, mu, cov_matrix):
         """plots the validation run, i.e. the true function, context points,
-        mean function and uncertainty """
-        random_function = np.random.randint(0, batch_size)
-        context_y_plot = funcvalues[
-                         random_function, contxt_idx, :
-                         ].flatten().cpu()
+        mean function and uncertainty
+        It makes also sure that the targetpoints and predictions are ordered
+        for propper plotting
+         """
 
-        context_x_plot = xvalues[random_function, contxt_idx, :].flatten().cpu()
-        y_plot = target_y[random_function, :, :].flatten().cpu()
-        x_plot = target_x[random_function, :, :].flatten().cpu()
-        mu_plot = mu[random_function, :, :].flatten().cpu()
-        var_plot = cov_matrix[random_function, :, :].flatten().cpu()
+        context_y_plot = funcvalues[:, contxt_idx, : ].flatten().cpu()
+        context_x_plot = xvalues[:, contxt_idx, :].flatten().cpu()
+        y_plot = target_y.flatten().cpu().numpy()
+        x_plot = target_x.flatten().cpu().numpy()
+        var_plot = cov_matrix.flatten().cpu().numpy()
+        mu_plot = mu.flatten().cpu().numpy()
+        x_plot, y_plot, mu_plot, var_plot = Helper.sort_arrays(
+            x_plot,
+            y_plot,
+            mu_plot,
+            var_plot)
+
         plt.scatter(x_plot, y_plot, color='red')
+        plt.plot(x_plot, mu_plot, color='blue')
         plt.scatter(context_x_plot, context_y_plot, color='black')
-        plt.scatter(x_plot, mu_plot, color='blue')
+
+
         plt.fill_between(x_plot, y1=mu_plot + var_plot,
                          y2=mu_plot - var_plot, alpha=0.2)
         plt.show()